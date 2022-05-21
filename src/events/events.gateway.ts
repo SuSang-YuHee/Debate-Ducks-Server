64e874b5413d87195ca5be2eab32a8b931294c0a
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-05-21 13:20:46 +0400

    [Add] WebRTC 적용

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -9,6 +9,7 @@ import {
   MessageBody,
 } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
+import Peer from "simple-peer";
 
 @WebSocketGateway({ cors: { origin: "*" } })
 export class EventsGateway
@@ -16,29 +17,49 @@ export class EventsGateway
 {
   @WebSocketServer() public server: Server;
 
-  @SubscribeMessage("join")
-  handleJoin(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody() debateId: string,
-  ) {
-    const roomSize = this.server.sockets.adapter.rooms.get(debateId)?.size || 0;
-    if (roomSize < 2) {
-      socket.join(debateId);
-      socket.to(debateId).emit("guestJoin");
-    } else {
-      socket.emit("overcapacity");
-    }
-  }
-
-  afterInit(): void {
+  afterInit() {
     console.log("WebSocket Server Init");
   }
 
-  handleConnection(@ConnectedSocket() socket: Socket): void {
+  handleConnection(@ConnectedSocket() socket: Socket) {
     console.log("connected", socket.id);
   }
 
-  handleDisconnect(@ConnectedSocket() socket: Socket): void {
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
     console.log("disconnected", socket.id);
+  }
+
+  // Room & WebRTC
+
+  @SubscribeMessage("join")
+  handleJoin(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() data: { debateId: string },
+  ) {
+    const roomSize =
+      this.server.sockets.adapter.rooms.get(data.debateId)?.size || 0;
+    if (roomSize < 2) {
+      socket.join(data.debateId);
+      socket.to(data.debateId).emit("guestJoin");
+    } else {
+      socket.emit("overcapacity");
+      socket.disconnect(); //*
+    }
+  }
+
+  @SubscribeMessage("offer")
+  handleOffer(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() data: { debateId: string; signal: Peer.SignalData },
+  ) {
+    socket.to(data.debateId).emit("offer", data.signal);
+  }
+
+  @SubscribeMessage("answer")
+  handleAnswer(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() data: { debateId: string; signal: Peer.SignalData },
+  ) {
+    socket.to(data.debateId).emit("answer", data.signal);
   }
 }
