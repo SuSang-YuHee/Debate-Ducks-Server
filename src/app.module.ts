1c29172dbd3ae8dfc76d306dbdd0c1a98f9c9c7a
sangbongPark <meca231@gmail.com>
2022-06-06 15:59:43 +0900

    [Add] app.module 유저 서비스 임포트

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EventsModule } from "./events/events.module";
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [EventsModule],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
