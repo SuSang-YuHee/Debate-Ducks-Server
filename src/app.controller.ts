0bcdb829640faf062a11f8121bb41c80799f4577
sangbongPark <meca231@gmail.com>
2022-06-07 20:04:52 +0900

    [Add] 환경설정 및 이메일 서비스 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/db-host-from-config")
+  getDatabaseHostFromConfigService(): string {
+    return this.configService.get("DATABASE_HOST");
   }
 }
