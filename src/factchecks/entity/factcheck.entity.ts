64f5bc3f7340c1a7696c3fafece708324faf1b66
sangbongPark <meca231@gmail.com>
2022-07-14 23:21:11 +0900

    [Add] 팩트체크 엔티티 생성 및 관계 설정

---
+++ src/factchecks/entity/factcheck.entity.ts
@@ -0,0 +1,27 @@
+import { DebateEntity } from "src/debates/entity/debate.entity";
+import { UserEntity } from "src/users/entity/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Factcheck")
+export class FactcheckEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne((type) => UserEntity, (target_user) => target_user.factchecks)
+  target_user: UserEntity;
+
+  @ManyToOne(
+    (type) => DebateEntity,
+    (target_debate) => target_debate.factchecks,
+  )
+  target_debate: DebateEntity;
+
+  @Column()
+  pros: boolean;
+
+  @Column()
+  description: string;
+
+  @Column()
+  reference_url: string;
+}
