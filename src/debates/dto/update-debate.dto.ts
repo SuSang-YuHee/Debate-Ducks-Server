ce519e076526b4ff402ac44173c6d6a94bc9d6be
sangbongPark <meca231@gmail.com>
2022-07-19 21:34:53 +0900

    [Modify] 업데이트 dto 수정

--- src/debates/dto/update-debate.dto.ts
+++ src/debates/dto/update-debate.dto.ts
@@ -1,13 +1,9 @@
 export class UpdateDebateDto {
   id: number;
-  author: string;
   title: string;
   contents: string;
   category: string;
   video_url: string;
-  participant: string;
+  participant_id: string;
   author_pros: boolean;
-  created_date: Date;
-  updated_date: Date;
-  ended_date: Date;
 }
