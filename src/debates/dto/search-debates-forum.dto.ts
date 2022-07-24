f07c6e0142a5ba771d69c0eab2bcc31c06ba01a7
sangbongPark <meca231@gmail.com>
2022-07-24 20:11:32 +0900

    [Update] dto에 Swagger 적용

--- src/debates/dto/search-debates-forum.dto.ts
+++ src/debates/dto/search-debates-forum.dto.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class SearchDebatesDto {
+  @ApiProperty({
+    example: "title=검색어",
+    description: "토론을 검색할 검색어에 해당합니다.",
+    required: true,
+  })
   title: string;
 }
