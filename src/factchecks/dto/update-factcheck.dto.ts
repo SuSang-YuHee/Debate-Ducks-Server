3728f12d0bce1b2d5a776b3ae946d0123dfd9715
sangbongPark <meca231@gmail.com>
2022-07-25 00:39:10 +0900

    [Update] Swagger 적용

--- src/factchecks/dto/update-factcheck.dto.ts
+++ src/factchecks/dto/update-factcheck.dto.ts
@@ -1,5 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class UpdateFactcheckDto {
+  @ApiProperty({
+    example: "1",
+    description: "수정할 팩트체크의 id입니다",
+    required: true,
+  })
   id: number;
+
+  @ApiProperty({
+    example: "testContents~~~",
+    description: "수정할 팩트체크의 설명, 내용 부분입니다.",
+    required: false,
+  })
   description: string;
+
+  @ApiProperty({
+    example: "testUrl",
+    description: "수정할 팩트체크의 설명, 내용을 뒷받침하는 자료의 링크입니다.",
+    required: false,
+  })
   reference_url: string;
 }
