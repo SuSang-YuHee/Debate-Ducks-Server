9c420d78fcf14b2820a8ce253a937a53c8b4cca7
sangbongPark <meca231@gmail.com>
2022-07-25 17:41:16 +0900

    [Modify] 유저 엔티티 프로필 사진 칼럼 수정

--- src/users/entity/user.entity.ts
+++ src/users/entity/user.entity.ts
@@ -20,7 +20,7 @@ export class UserEntity {
   @Column({ length: 30, select: false })
   password: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, select: false })
   profile_image: string;
 
   @Column({ length: 60, select: false })
