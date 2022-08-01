17055a3fb3ec4b1c2c07a2071b145e56f7dff468
sangbongPark <meca231@gmail.com>
2022-08-02 00:02:17 +0900

    [Update] 댓글 리스트 조회 dto Swagger 설정 추가

--- src/comments/dto/get-comments-response.dto.ts
+++ src/comments/dto/get-comments-response.dto.ts
@@ -1,6 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { CommentEntity } from "../entities/comment.entity";
 
 export class GetCommentsResponseDto {
+  @ApiProperty({
+    description: "댓글 리스트입니다.",
+  })
   list: CommentEntity[];
+
+  @ApiProperty({
+    description: "댓글 리스트를 조회할 때 마지막 페이지인지의 여부입니다.",
+  })
   isLast: boolean;
 }
