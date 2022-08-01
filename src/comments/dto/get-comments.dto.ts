2ebaa00f3941e5db20e103b3c57900748b2e5034
sangbongPark <meca231@gmail.com>
2022-08-02 00:04:23 +0900

    [Update] 댓글 리스트 조회 dto 유효성 검사 추가

--- src/comments/dto/get-comments.dto.ts
+++ src/comments/dto/get-comments.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsOptional } from "class-validator";
 
 export class GetCommentsDto {
   @ApiProperty({
@@ -6,6 +7,8 @@ export class GetCommentsDto {
     description: "조회 할 리스트의 페이지입니다.",
     required: false,
   })
+  @IsNumber()
+  @IsOptional()
   page: number;
 
   @ApiProperty({
@@ -13,6 +16,7 @@ export class GetCommentsDto {
     description: "조회 할 리스트의 정렬입니다.",
     required: false,
   })
+  @IsOptional()
   order: "ASC" | "DESC";
 
   @ApiProperty({
@@ -20,5 +24,7 @@ export class GetCommentsDto {
     description: "리스트의 목록 개수입니다.",
     required: false,
   })
+  @IsNumber()
+  @IsOptional()
   take: number;
 }
