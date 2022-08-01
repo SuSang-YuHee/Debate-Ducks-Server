bc9f7ff31870737a3813b4164a1681d34a699e97
sangbongPark <meca231@gmail.com>
2022-08-01 23:55:39 +0900

    [Modify] 토론 리스트 조회 dto 유효성 검사 추가

--- src/debates/dto/get-debates-forum.dto.ts
+++ src/debates/dto/get-debates-forum.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 
 export class GetDebatesDto {
   @ApiProperty({
@@ -6,6 +7,8 @@ export class GetDebatesDto {
     description: "토론을 검색할 검색어에 해당합니다.",
     required: false,
   })
+  @IsString()
+  @IsOptional()
   title: string;
 
   @ApiProperty({
@@ -13,6 +16,7 @@ export class GetDebatesDto {
     description: "토론 리스트 정렬에 해당합니다.",
     required: false,
   })
+  @IsOptional()
   order: "ASC" | "DESC";
 
   @ApiProperty({
@@ -20,6 +24,8 @@ export class GetDebatesDto {
     description: "토론 리스트에 담을 토론 개수에 해당합니다.",
     required: false,
   })
+  @IsNumber()
+  @IsOptional()
   count: number;
 
   @ApiProperty({
@@ -27,5 +33,7 @@ export class GetDebatesDto {
     description: "토론 리스트의 페이지에 해당합니다.",
     required: false,
   })
+  @IsNumber()
+  @IsOptional()
   page: number;
 }
