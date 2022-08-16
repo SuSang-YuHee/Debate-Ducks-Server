50db27f25856325602004c5eb2d85f991aaf3f25
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-08-17 08:49:35 +0900

    [Modify] event module 주석 달기

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -28,6 +28,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
       this.server.sockets.adapter.rooms.get(data.debateId)?.size || 0;
 
     if (roomSize < 2 && !roomId[socket.id]) {
+      //* 2명까지 입장 가능, 중복 입장 방지
       roomId[socket.id] = data.debateId;
       roomInfo[data.debateId] = roomInfo[data.debateId] || {
         size: 0,
@@ -42,11 +43,14 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
       socket.join(data.debateId);
       socket.to(data.debateId).emit("peerJoin");
     } else if (roomId[socket.id]) {
+      //* 중복 입장 시 거절 방지
     } else {
+      //* 2인 초과 시 거절
       socket.emit("overcapacity");
     }
   }
 
+  //* WebRTC 제안
   @SubscribeMessage("offer")
   handleOffer(
     @ConnectedSocket() socket: Socket,
@@ -55,6 +59,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
     socket.to(data.debateId).emit("offer", data.signal);
   }
 
+  //* WebRTC 응답
   @SubscribeMessage("answer")
   handleAnswer(
     @ConnectedSocket() socket: Socket,
@@ -65,14 +70,17 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
 
   //*- 연결 해제
   handleDisconnect(@ConnectedSocket() socket: Socket) {
+    //* 룸 아이디 획득 후 삭제
     if (!roomId[socket.id]) return;
     const debateId = roomId[socket.id];
     delete roomId[socket.id];
 
+    //* 퇴장 처리
     if (!roomInfo[debateId]) return;
     roomInfo[debateId].size -= 1;
     socket.to(debateId).emit("peerDisconnect");
 
+    //* 마지막 퇴장 시 토론 중단 및 삭제
     if (roomInfo[debateId].size >= 1) return;
     if (roomInfo[debateId].debate) {
       clearInterval(roomInfo[debateId].debate);
@@ -81,6 +89,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
   }
 
   //*- 정보 송수신
+  //* 상대 비디오 끄기/켜기 정보
   @SubscribeMessage("peerVideo")
   handlePeerVideo(
     @ConnectedSocket() socket: Socket,
@@ -89,6 +98,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
     socket.to(data.debateId).emit("peerVideo", data.isVideoOn);
   }
 
+  //* 상대 화면 공유 끄기/켜기 정보
   @SubscribeMessage("peerScreen")
   handlePeerScreen(
     @ConnectedSocket() socket: Socket,
@@ -97,6 +107,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
     socket.to(data.debateId).emit("peerScreen", data.isScreenOn);
   }
 
+  //* 토론 시작 중비 정보
   @SubscribeMessage("ready")
   handleReady(
     @ConnectedSocket() socket: Socket,
@@ -124,6 +135,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
     }
   }
 
+  //* 2분 미만이며 자신의 차례일 경우 넘기기
   @SubscribeMessage("skip")
   handleSkip(@MessageBody() data: { debateId: string; isPros: boolean }) {
     if (!roomInfo[data.debateId]) return;
@@ -153,6 +165,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
     if (roomInfo[data.debateId]) {
       clearInterval(roomInfo[data.debateId].debate);
       delete roomInfo[data.debateId];
+      //* 먼저 요청온 사용자만 업로드 로직 실행
       socket.emit("debateDone", true);
       socket.to(data.debateId).emit("debateDone", false);
     }
